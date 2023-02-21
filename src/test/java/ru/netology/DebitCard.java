fcc5da68b68ca655eac4af4cf48870f8cd5ae92d
Evgeny Katukhov <e.katuhov@gmail.com>
2023-02-21 19:49:50 +0300

    src

---
+++ src/test/java/ru/netology/DebitCard.java
@@ -0,0 +1,54 @@
+package ru.netology;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DebitCard {
+    private WebDriver driver;
+
+    @BeforeAll
+            public static void setup() {
+        WebDriverManager.chromedriver().setup();
+    }
+    @BeforeEach
+            public void beforeEach() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+    @AfterEach
+    public void afterEach() {
+        driver.quit();
+        driver = null;
+    }
+    @Test // Задача 1.
+    public void shouldSendForm() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Кузнецова Анастасия");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79109454927");
+        driver.findElement(By.cssSelector("[data-test-id=agreement] input")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+
+        String expectedText = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actualText = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+
+        assertEquals(expectedText, actualText);
+
+    }
+
+
+
+
+}
